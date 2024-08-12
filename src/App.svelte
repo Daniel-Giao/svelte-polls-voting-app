3cf33adbd94dcbaaf948c93860a401694e8d8695
Daniel Giao <daniel.giao@icloud.com>
2024-08-12 18:01:44 -0400

    Added Header & Footer component

--- src/App.svelte
+++ src/App.svelte
@@ -1,28 +1,22 @@
 <script>
-
+  // components import
+  import Header from "./components/Header.svelte";
+  import Footer from "./components/Footer.svelte";
 </script>
 
+<Header />
 <main>
+  <p>
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt quo nisi
+    accusamus soluta odio. Atque dignissimos blanditiis repudiandae nobis eos
+    tempore, nisi, molestias commodi, ipsum maxime eaque nulla ducimus quo.
+  </p>
 </main>
+<Footer />
 
 <style>
   main {
-    text-align: center;
-    padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
-  }
-
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
-    }
+    max-width: 960px;
+    margin: 40px auto;
   }
 </style>
