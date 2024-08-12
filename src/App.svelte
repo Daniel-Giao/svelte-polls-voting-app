213698dc978ca303619fec8c34f25041046dde91
Daniel Giao <daniel.giao@icloud.com>
2024-08-12 18:20:35 -0400

    Added tabs for Current Polls & Add New Poll

--- src/App.svelte
+++ src/App.svelte
@@ -2,15 +2,24 @@
   // components import
   import Header from "./components/Header.svelte";
   import Footer from "./components/Footer.svelte";
+  import Tabs from "./shared/Tabs.svelte";
+
+  // tabs
+  let items = ["Current Polls", "Add New Poll"];
+  let activeItem = "Current Polls";
+  const tabChange = (e)=>{
+    activeItem = e.detail
+  }
 </script>
 
 <Header />
 <main>
-  <p>
-    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt quo nisi
-    accusamus soluta odio. Atque dignissimos blanditiis repudiandae nobis eos
-    tempore, nisi, molestias commodi, ipsum maxime eaque nulla ducimus quo.
-  </p>
+  <Tabs {activeItem} {items} on:tabChange={tabChange}/>
+  {#if activeItem === 'Current Polls'}
+    <p>Poll list component goes here</p>
+  {:else if activeItem === 'Add New Poll'}
+    <p>new poll form component goes here</p>
+  {/if}
 </main>
 <Footer />
 
