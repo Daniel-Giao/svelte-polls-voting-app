136d8861c57bf2b1ec3ae204df58bd10730b5d5e
Daniel Giao <daniel.giao@icloud.com>
2024-08-12 22:24:31 -0400

    Added tweened effect to poll bars

--- src/components/PollDetails.svelte
+++ src/components/PollDetails.svelte
@@ -1,4 +1,7 @@
 <script>
+  // tweens import
+  import { tweened } from "svelte/motion";
+
   // stores import
   import PollStore from "../stores/PollStore";
 
@@ -7,10 +10,21 @@
   import Button from "../shared/Button.svelte";
 
   export let poll;
-  $: totalVotes = poll.votesA + poll.votesB;
-  $: percentA = Math.floor((100 / totalVotes) * poll.votesA);
-  $: percentB = Math.floor((100 / totalVotes) * poll.votesB);
 
+  // reactive values
+  $: totalVotes = poll.votesA + poll.votesB;
+  $: percentA = Math.floor((100 / totalVotes) * poll.votesA) || 0;
+  $: percentB = Math.floor((100 / totalVotes) * poll.votesB) || 0;
+
+  // tweened percentages
+  const tweenedA = tweened(0);
+  const tweenedB = tweened(0);
+  $: tweenedA.set(percentA);
+  $: tweenedB.set(percentB);
+  $: console.log($tweenedA, $tweenedB);
+  
+
+  // handling votes
   const handleVote = (option, id) => {
     PollStore.update((currentPolls) => {
       let copiedPolls = [...currentPolls];
@@ -38,11 +52,11 @@
     <h3>{poll.question}</h3>
     <p>Total Votes: {totalVotes}</p>
     <div class="answer" on:click={() => handleVote("a", poll.id)}>
-      <div class="percent percent-a" style="width: {percentA}%;"></div>
+      <div class="percent percent-a" style="width: {$tweenedA}%;"></div>
       <span>{poll.answerA} ({poll.votesA})</span>
     </div>
     <div class="answer" on:click={() => handleVote("b", poll.id)}>
-      <div class="percent percent-b" style="width: {percentB}%;"></div>
+      <div class="percent percent-b" style="width: {$tweenedB}%;"></div>
       <span>{poll.answerB} ({poll.votesB})</span>
     </div>
     <div class="delete">
