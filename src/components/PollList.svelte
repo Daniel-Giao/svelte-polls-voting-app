6c62a1ef1a7430f390385a76c2f35400099d11d8
Daniel Giao <daniel.giao@icloud.com>
2024-08-12 22:08:02 -0400

    Added transition and animate effects from svelte

--- src/components/PollList.svelte
+++ src/components/PollList.svelte
@@ -1,17 +1,19 @@
 <script>
+  // transition & animate imports
+  import { fade, slide, scale } from "svelte/transition";
+  import { flip } from "svelte/animate";
+
   // stores import
   import PollStore from "../stores/PollStore";
 
   // components import
   import PollDetails from "./PollDetails.svelte";
-
-  export let polls = [];
 </script>
 
 <div class="poll-list">
   <!-- Automatic unsub -->
   {#each $PollStore as poll (poll.id)}
-    <div>
+    <div in:fade out:scale|local animate:flip={{ duration: 500 }}>
       <PollDetails {poll} />
     </div>
   {/each}
